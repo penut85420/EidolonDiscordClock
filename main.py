0f35dc2114a5bcf4849f1f7d943cb9e42b6a4727
PenutChen <penut_chen@cyberon.com.tw>
2022-09-04 19:33:38 +0800

    Use Cog

--- main.py
+++ main.py
@@ -1,66 +1,78 @@
+import asyncio
 import os
 import sys
-import asyncio
-import discord
 
+import discord
+from discord.ext import commands, tasks
 from loguru import logger
+
 from alarm import AlarmMan
 
-class EidolonBot(discord.Client):
+
+class EidolonBot(commands.Bot):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
     async def on_ready(self):
-        logger.info(f'{self.user} | Ready!')
+        logger.info(f"{self.user} | Ready!")
 
-        reset = 1000
-        am = AlarmMan()
-        alarm_channel_id = os.getenv('ALARM_CHANNEL').split(',')
+        alarm_channel_id = os.getenv("ALARM_CHANNEL").split(",")
         alarm_channel_id = set(list(map(int, alarm_channel_id)))
 
-        while not self.is_closed():
-            for channel in self.get_all_channels():
-                if channel.id in alarm_channel_id:
-                    if channel.last_message_id is None:
-                        await channel.send(am.full_message())
+        for channel in self.get_all_channels():
+            if channel.id in alarm_channel_id:
+                self.add_cog(ChannelAlarm(channel, self.user))
 
-                    msg = await channel.fetch_message(channel.last_message_id)
-                    if msg.author == self.user:
-                        try:
-                            if msg.content != am.full_message():
-                                await msg.edit(content=am.full_message())
 
-                            await asyncio.sleep(3)
+class ChannelAlarm(commands.Cog):
+    def __init__(self, channel, user):
+        self.channel = channel
+        self.user = user
+        self.am = AlarmMan()
+        self.printer.start()
+        self.reset = 1000
 
-                            reset -= 1
-                            if reset < 0:
-                                logger.info(am.refresh())
-                                reset = 1000
-                        except Exception as e:
-                            logger.error(f'Error: {str(e)}')
+    def cog_unload(self):
+        self.printer.cancel()
 
-    async def on_message(self, msg):
+    @tasks.loop(seconds=60)
+    async def printer(self):
+        if self.channel.last_message_id is None:
+            await self.channel.send(self.am.full_message())
+
+        msg = await self.channel.fetch_message(self.channel.last_message_id)
         if msg.author == self.user:
-            return
+            try:
+                if msg.content != self.am.full_message():
+                    await msg.edit(content=self.am.full_message())
+
+                await asyncio.sleep(3)
+
+                self.reset -= 1
+                if self.reset < 0:
+                    logger.info(self.am.refresh())
+                    self.reset = 1000
+            except Exception as e:
+                logger.error(f"Error: {str(e)}")
+
 
 def set_logger():
     log_format = (
-        '{time:YYYY-MM-DD HH:mm:ss.SSSSSS} | '
-        '<lvl>{level: ^9}</lvl> | '
-        '{message}'
+        "{time:YYYY-MM-DD HH:mm:ss.SSSSSS} | <lvl>{level: ^9}</lvl> | {message}"
     )
-    logger.add(sys.stderr, level='INFO', format=log_format)
+    logger.add(sys.stderr, level="INFO", format=log_format)
     logger.add(
-        f'./logs/bot.log',
-        rotation='1 day',
-        retention='7 days',
-        level='INFO',
-        encoding='UTF-8',
-        compression='gz',
-        format=log_format
+        f"logs/bot.log",
+        rotation="1 day",
+        retention="7 days",
+        level="INFO",
+        encoding="UTF-8",
+        compression="gz",
+        format=log_format,
     )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     set_logger()
-    token = os.getenv('TOKEN')
-    EidolonBot().run(token)
+    token = os.getenv("TOKEN")
+    EidolonBot(command_prefix="!").run(token)
