adc0b3536eebd3166b4dbe2f6ebb571b8392f7e4
PenutChen <penut_chen@cyberon.com.tw>
2022-09-16 20:34:47 +0800

    Improve Error Handle

--- cetus_api.py
+++ cetus_api.py
@@ -1,39 +1,50 @@
 import re
 import json
+import time
 import requests
 import datetime as dt
 
+
 def get_time_string():
-    url = 'https://api.warframestat.us/pc/cetusCycle'
+    url = "https://api.warframestat.us/pc/cetusCycle"
     r = json.loads(requests.get(url).content)
 
     # Match Time Format
-    m = re.search(r'(\d)h (\d+)m (\d+)s', r['timeLeft'])
+    m = re.search(r"(\d)h (\d+)m (\d+)s", r["timeLeft"])
     hh, mm, ss = map(int, m.groups())
     mm = hh * 60 + mm
 
     # Match State
-    state = '入夜' if r['isDay'] else '日出'
+    state = "入夜" if r["isDay"] else "日出"
 
     # Calculate Next Night
     d = dt.timedelta(minutes=mm, seconds=ss)
     d = dt.datetime.now() + d
     d = d.replace(second=0, microsecond=0)
-    d = d.strftime('%Y-%m-%d %H:%M:%S')
+    d = d.strftime("%Y-%m-%d %H:%M:%S")
 
-    return f'希圖斯還有 {mm} 分鐘{state}，下個夜晚：{d}'
+    return f"希圖斯還有 {mm} 分鐘{state}，下個夜晚：{d}"
+
 
 def get_time():
-    url = 'https://api.warframestat.us/pc/cetusCycle'
-    r = json.loads(requests.get(url).content)
+    url = "https://api.warframestat.us/pc/cetusCycle"
+    r = None
+    while r is None:
+        try:
+            r = json.loads(requests.get(url).content)
+        except:
+            print("Cetus API Get Time Failed")
+            r = None
+            time.sleep(10)
 
     # Match Time Format
-    m = re.search(r'((\d)h )?((\d+)m )?(\d+)s', r['timeLeft'])
+    m = re.search(r"((\d)h )?((\d+)m )?(\d+)s", r["timeLeft"])
     _, hh, _, mm, ss = m.groups()
     to_int = lambda x: 0 if x is None else int(x)
     hh, mm, ss = map(to_int, (hh, mm, ss))
     mm = hh * 60 + mm
-    if not r['isDay']: mm += 100
+    if not r["isDay"]:
+        mm += 100
 
     # Calculate Last Day Night
     d = dt.timedelta(minutes=mm, seconds=ss)
@@ -41,6 +52,7 @@ def get_time():
     d = d.replace(second=0, microsecond=0)
 
     return d
+
 
 if __name__ == "__main__":
     print(get_time())
